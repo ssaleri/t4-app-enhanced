4a513f9009bc04240839e5557c87116e1e757ad3
Simone Saleri <simonesaleri@hotmail.it>
2024-03-02 11:59:24 +0100

    ci: add icons to bottom bar tabs

---
+++ apps/expo/app/(tabs)/Protected/[id]/index.tsx
@@ -0,0 +1,11 @@
+import { ParamsScreen } from 'app/features/params/screen'
+import { Stack } from 'expo-router'
+
+export default function Screen() {
+  return (
+    <>
+      <Stack.Screen/>
+      <ParamsScreen />
+    </>
+  )
+};
--- apps/expo/app/(tabs)/_layout.tsx
+++ apps/expo/app/(tabs)/_layout.tsx
@@ -1,16 +1,40 @@
 import { Tabs } from "expo-router/tabs";
+import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons';
 
 export default function Layout() {
   return (
     <Tabs screenOptions={{headerShown: false}}>
       <Tabs.Screen
         name="Blog"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <SimpleLineIcons name="feed" size={size} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="About"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <SimpleLineIcons name="user" size={size} color={color} />
+          ),
+        }}
       />
       <Tabs.Screen
         name="Portfolio"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <SimpleLineIcons name="briefcase" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Protected"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <SimpleLineIcons name="lock" size={size} color={color} />
+          ),
+        }}
       />
     </Tabs>
   )
